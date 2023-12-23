1105f3d72d28fe031894b36fcd69cd2fc5c22e7a
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-12-23 10:41:53 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
       <span className="animate-bounce">🏀</span>
       <h1 className="animate-pulse">
-        Welcome to Session 1 of the Clarus Store Project FS15!
+        Welcome to Session 1 of the Clarus Store Project FS15!sgfdb
       </h1>
       <span className="animate-bounce">🏀</span>
     </div>
