027d2cf8a464de85008cbd12e11e9b6b531efc6c
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-12-23 14:15:15 +0300

    hamburger menu visibility

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import AppRouter from "./router/AppRouter";
+import AuthProvider from "./context/AuthProvider";
 
 function App() {
   return (
-    
-    <AppRouter />
-    
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
   );
 }
 
